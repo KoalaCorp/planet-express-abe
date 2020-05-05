d971e4c6e6b1f8c3e65faaad70914a2609d1c799
ewokcillo <azotillo@gmail.com>
2020-05-05 22:03:06 +0200

    Add tests Add github tests actions Add insert neighbours in linkstopics endpoint Add degree parameter to get X levels of neighbours Refactor by classes in mongo.py module Add populate script for database sources collection

--- src/mongo.py
+++ src/mongo.py
@@ -3,76 +3,11 @@ from pymongo import MongoClient
 
 class Mongo(object):
     def __init__(self, database, host, port):
-        client = MongoClient(host, port)
-        self.database = client[database]
-
-    def get_links_topics(self, source, queries):
-        sources_collection = self.database['sources']
-        source_data = sources_collection.find_one({"id": source}, {'_id': False})
-        collection = self.database[source]
-        iterator_docs = collection.find(
-            {
-                "$or": [
-                    {"tokenized.topics.word": {"$in": queries}},
-                    {"tokenized.names.word": {"$in": queries}}
-                ]
-            },
-            {'_id': False}
-        )
-
-        links_dict = {}
-        topics_dict = {}
-        date_time = "date_time"
-        title = "title"
-        for doc in iterator_docs:
-            doc_keys = doc.keys()
-            url = {
-                title: doc[title] if title in doc_keys else "",
-                "date": doc[date_time] if date_time in doc_keys else "",
-                "href": doc['url'],
-                "source": {
-                    "label": source_data["id"],
-                    "name": source_data["name"]
-                }
-            }
-            for token in doc['tokenized']:
-                url_topics = []
-                for topic in token['topics']:
-                    topic_word = topic['word']
-                    topic_keys = topics_dict.keys()
-                    links_keys = links_dict.keys()
-
-                    for url_topic in url_topics:
-                        sorted_topics_name_list = [url_topic, topic_word]
-                        sorted_topics_name = "_".join(sorted_topics_name_list)
-                        if sorted_topics_name not in links_keys:
-                            links_dict[sorted_topics_name] = {
-                                "endpoints": sorted_topics_name_list,
-                                "urls": [url]
-                            }
-                        else:
-                            links_dict[sorted_topics_name]["urls"].append(url)
-                    url_topics.append(topic_word)
-
-                    if topic_word not in topic_keys:
-                        topics_dict[topic_word] = {
-                            "type": "topics",
-                            "id": topic_word,
-                            "attributes": {
-                                "label": topic_word
-                            },
-                            "meta": {
-                                "counter": topic['score']
-                            }
-                        }
-                    else:
-                        topics_dict[topic['word']]['meta']['counter'] += topic['score']
+        self.client = MongoClient(host, port)
+        self.database = self.client[database]
 
 
-        links = [val for key, val in links_dict.items()]
-        topics = [val for key, val in topics_dict.items()]
-        return topics, links
-
+class SourcesMongo(Mongo):
     def get_sources(self):
         collection = self.database['sources']
         iterator_docs = collection.find({}, {'_id': False})
@@ -90,6 +25,8 @@ class Mongo(object):
 
         return data
 
+
+class SourceMongo(Mongo):
     def get_source(self, source):
         collection = self.database['sources']
         source_data = collection.find_one({"id": source}, {'_id': False})
@@ -105,3 +42,96 @@ class Mongo(object):
         }
 
         return return_data
+
+
+class LinksTopicsMongo(Mongo):
+    date_time = "date_time"
+    title = "title"
+
+    def __init__(self, database, host, port):
+        self.links_dict = {}
+        self.topics_dict = {}
+        self.exclude_ids = []
+        self.queries = None
+        Mongo.__init__(self, database, host, port)
+
+    def __doc_to_url(self, doc, source_data):
+        doc_keys = doc.keys()
+        return {
+            self.title: doc[self.title] if self.title in doc_keys else "",
+            "date": doc[self.date_time] if self.date_time in doc_keys else "",
+            "href": doc['url'],
+            "source": {
+                "label": source_data["id"],
+                "name": source_data["name"]
+            }
+        }
+
+    def __doc_to_topic__(self, topic_word, score):
+        return {
+            "type": "topics",
+            "id": topic_word,
+            "attributes": {
+                "label": topic_word
+            },
+            "meta": {
+                "counter": score
+            }
+        }
+
+    def __doct_to_link__(self, sorted_topics_name_list, url):
+        return {
+            "endpoints": sorted_topics_name_list,
+            "urls": [url]
+        }
+
+    def __iterate_docs__(self, iterator_docs, source_data):
+        for doc in iterator_docs:
+            self.exclude_ids.append(doc['_id'])
+            url = self.__doc_to_url(doc, source_data)
+            for token in doc['tokenized']:
+                url_topics = []
+                for topic in token['topics'] + token['names']:
+                    topic_word = topic['word']
+                    self.queries.append(topic_word)
+                    for url_topic in url_topics:
+                        sorted_topics_name_list = [url_topic, topic_word]
+                        sorted_topics_name = "_".join(sorted_topics_name_list)
+                        if sorted_topics_name not in self.links_dict.keys():
+                            self.links_dict[sorted_topics_name] =\
+                                self.__doct_to_link__(sorted_topics_name_list,
+                                                      url)
+                        else:
+                            self.links_dict[sorted_topics_name]["urls"]\
+                                .append(url)
+                    url_topics.append(topic_word)
+
+                    if topic_word not in self.topics_dict.keys():
+                        self.topics_dict[topic_word] =\
+                            self.__doc_to_topic__(topic_word, topic['score'])
+                    else:
+                        self.topics_dict[topic['word']]['meta']['counter'] +=\
+                            topic['score']
+
+    def get_links_topics(self, source, queries, degrees):
+        sources_collection = self.database['sources']
+        source_data = sources_collection.find_one({"id": source})
+        collection = self.database[source]
+        self.queries = queries
+
+        for degree in range(degrees):
+            iterator_docs = collection.find(
+                {
+                    "_id": {"$nin": self.exclude_ids},
+                    "$or": [
+                        {"tokenized.topics.word": {"$in": self.queries}},
+                        {"tokenized.names.word": {"$in": self.queries}}
+                    ]
+                }
+            )
+
+            self.__iterate_docs__(iterator_docs, source_data)
+
+        links = [val for key, val in self.links_dict.items()]
+        topics = [val for key, val in self.topics_dict.items()]
+        return topics, links
