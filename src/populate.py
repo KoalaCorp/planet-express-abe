e29fca9426335bc6d2f8ff1d600e74ca5df15a63
ewokcillo <azotillo@gmail.com>
2020-05-05 22:17:57 +0200

    Remove github character from populate.py script

--- src/populate.py
+++ src/populate.py
@@ -2,10 +2,7 @@ import argparse
 import json
 
 from mongo import Mongo
-<<<<<<< HEAD
 from settings import MONGO_DATABASE, MONGO_HOST, MONGO_PORT
-=======
->>>>>>> 417c930693db0ce39bfbd47b3ee4abd9021f754d
 
 
 def populate(mongo_database, mongo_host, mongo_port):
@@ -18,7 +15,6 @@ def populate(mongo_database, mongo_host, mongo_port):
 
 
 if __name__ == '__main__':
-<<<<<<< HEAD
     parser = argparse.ArgumentParser(
         description='Populate the database whit initial data')
     parser.add_argument('--mongo_database', help='Mongo database name',
@@ -27,13 +23,5 @@ if __name__ == '__main__':
                         default=MONGO_HOST)
     parser.add_argument('--mongo_port', help='Mongo port',
                         default=MONGO_PORT)
-=======
-    parser = argparse.ArgumentParser(description='Populate the database whit initial data')
-    parser.add_argument('--mongo_database', help='Mongo database name')
-    parser.add_argument('--mongo_host', help='Type of text to tokenize',
-                        default='text')
-    parser.add_argument('--mongo_port', help='PDF paragraph text separator',
-                        default='  \x0c')
->>>>>>> 417c930693db0ce39bfbd47b3ee4abd9021f754d
     args = parser.parse_args()
     populate(args.mongo_database, args.mongo_host, args.mongo_port)
